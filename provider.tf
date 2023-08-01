7773e17107e052482bcfcadd299f2a125b7e878e
kiranpoloju27 <kiran.poloju27@gmail.com>
2023-08-01 15:33:48 -0400

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region = "us-east-1" # Change this to your desired AWS region"
+  access_key = "AKIAXEKDLGO23W7JL6NW"
+  secret_key = "Ny1/Az8br7r9CwQkB2P12IVUP/3r1vKpWtyi0+eo"
+}
---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "zone_id" {
+  description = "The Route53 hosted zone ID to enable query logs."
+  default = "Z03309881PN2JZ6GGLOJ8"
+}
+
+variable "route53_zone_name" {
+  description = "The Route53 hosted zone name."
+  default = "axle-interns.com"
+}
