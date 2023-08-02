035bc5fb9d82cdade56a057dab903a271081267b
kiranpoloju27 <kiran.poloju27@gmail.com>
2023-08-02 10:11:11 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
   region = "us-east-1" # Change this to your desired AWS region"
-  access_key = "AKIAXEKDLGO23W7JL6NW"
-  secret_key = "Ny1/Az8br7r9CwQkB2P12IVUP/3r1vKpWtyi0+eo"
+  
 }
